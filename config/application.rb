cde0cb1179c03b986b459966513b5caa15b524f3
Jeremías <jimmyguionbajo@gmail.com>
2019-10-24 09:22:53 -0300

    added rack cors

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
@@ -23,7 +23,6 @@ module ParkingControlApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -33,5 +32,15 @@ module ParkingControlApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "*"
+        resource(
+          "*",
+          headers: :any,
+          methods: [:get, :patch, :put, :delete, :post, :options],
+        )
+      end
+    end
   end
 end
